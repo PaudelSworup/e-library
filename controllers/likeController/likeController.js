d138ac9e4820c73fb9f967f3d228808d1c72a9e0
PaudelSworup <sworupkc9@gmail.com>
2023-05-27 14:40:52 +0545

    likeuodated

--- controllers/likeController/likeController.js
+++ controllers/likeController/likeController.js
@@ -1,9 +1,19 @@
 const Like = require("../../models/Like/LIkeModel");
 
 exports.postLikes = async (req, res) => {
-  let likes = new Like({
+  const existingLike = await Like.findOne({
     book: req.body.book,
     user: req.body.user,
+  });
+
+  if (existingLike) {
+    await Like.findByIdAndDelete(existingLike?._id);
+    return res.status(200).send({success:true , message:"Unliked"})
+  }
+
+  let likes = new Like({
+    book: req.body.book,
+    user: req.body.user,  
   });
   likes.likedStatus = 1;
   likes = await likes.save();
@@ -33,27 +43,22 @@ exports.getLikes = async (req, res) => {
   const counts = await Like.aggregate([
     {
       $group: {
-        _id: '$book',
-        count: { $sum: 1 }
-      }
+        _id: "$book",
+        count: { $sum: 1 },
+      },
     },
     {
       $match: {
-        count: { $gte: 1 }
-      }
-    }
+        count: { $gte: 1 },
+      },
+    },
   ]);
-
-console.log(counts);
 
   return res.status(200).send({
     success: true,
     likes,
-    counts
+    counts,
   });
 };
 
-exports.getLikedCount = async (req, res) => {
-  
-   
-};
+
