e45c6d13928ae94935418cd31a515f61f8e936f3
PaudelSworup <sworupkc9@gmail.com>
2023-05-27 15:47:06 +0545

    updatedlikes

--- routes/likeRoute/likeRoute.js
+++ routes/likeRoute/likeRoute.js
@@ -1,10 +1,9 @@
 const express = require("express")
-const { postLikes, getLikes, getLikedCount } = require("../../controllers/likeController/likeController")
+const { postLikes, getLikes } = require("../../controllers/likeController/likeController")
 const router = express.Router()
 
 
 router.post("/like" , postLikes)
 router.get("/like" , getLikes)
-router.get("/like/:book" , getLikedCount)
 
 module.exports = router
